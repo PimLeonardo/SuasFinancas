0619e7923405adb8bb03b6d2a174a94003aee9cb
Leonardo Pimentel <leozito4532@gmail.com>
2021-06-23 21:10:39 -0300

    Aplicando id

--- app.js
+++ app.js
@@ -7,7 +7,35 @@ class Despesas {
 		this.valor = valor
 		this.descricao = descricao
 	}
+
+	validarDados()	{
+		for()
+	}
 }
+
+class Bd {
+
+	constructor() {
+		let id = localStorage.getItem('id')
+
+		if (id === null) {
+			localStorage.setItem('id', 0)
+		}
+	}
+
+	getId(){
+		let novoId = localStorage.getItem('id')
+		return parseInt(novoId) + 1
+	}
+
+	salvar(d) {
+		let id = this.getId()
+		localStorage.setItem(id, JSON.stringify(d))
+		localStorage.setItem('id', id)
+	}
+}
+
+let bd = new Bd()
 
 function cadastrarDespesa() {
 
@@ -20,9 +48,11 @@ function cadastrarDespesa() {
 
 	let despesas = new Despesas(dia.value, mes.value, ano.value, tipo.value, valor.value, descricao.value)
 
-	salvar(despesas)
+	if (despesas.validarDados()) {
+		bd.salvar(despesas)
+	} else {
+
+	}
+	
 }
 
-function salvar(d) {
-	localStorage.setItem('despesas', JSON.stringify(d))
-}
