c702288c180c7dba703a01ed1c0350b57a0cf6ff
Leonardo Pimentel <leozito4532@gmail.com>
2021-06-26 16:13:32 -0300

    Mensagem de erro

--- app.js
+++ app.js
@@ -9,7 +9,13 @@ class Despesas {
 	}
 
 	validarDados()	{
-		for()
+
+		for(let i in this){
+			if (this[i] == undefined || this[i] == '' || this[i] == null || this.dia > 31 || this.dia < 1) {
+				return false
+			} 
+		}
+		return true
 	}
 }
 
@@ -33,6 +39,22 @@ class Bd {
 		localStorage.setItem(id, JSON.stringify(d))
 		localStorage.setItem('id', id)
 	}
+
+	recuperarDados() {
+		let id = localStorage.getItem('id')
+		let despesas = Array()
+
+		for(let i = 1; i <= id; i++) {
+			let despesa = JSON.parse(localStorage.getItem(i))
+
+			if (despesa === null) {
+				continue
+			}
+
+			despesas.push(despesa)
+		}
+		return despesas
+	}
 }
 
 let bd = new Bd()
@@ -49,10 +71,20 @@ function cadastrarDespesa() {
 	let despesas = new Despesas(dia.value, mes.value, ano.value, tipo.value, valor.value, descricao.value)
 
 	if (despesas.validarDados()) {
-		bd.salvar(despesas)
-	} else {
+		//bd.salvar(despesas)
 
+		document.getElementById('modal_titulo').innerHTML = 'Cadastro de despesa realizado'
+		document.getElementById('modal_mensagem').innerHTML = 'Sua despesa foi cadastrada com sucesso.'
+		$('#modalregistro').modal('show')
+	} else {
+		document.getElementById('modal_titulo').innerHTML = 'Erro no cadastro de despesa'
+		document.getElementById('modal_mensagem').innerHTML = 'Existem campos que não foram preenchidos de maneira correta.'
+		$('#modalregistro').modal('show')
 	}
-	
 }
 
+function carregarDespesas() {
+	let despesas = Array()
+
+	despesas = bd.recuperarDados()
+}
